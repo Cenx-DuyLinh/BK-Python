09aca26519aa784ca1311a2a3e734f4e7bc511d0
Duy Linh <duylinh.tran98@gmail.com>
2023-04-30 15:54:08 +0700

    add bai 3, 4, 5

---
+++ bai_4.py
@@ -0,0 +1,10 @@
+print('Hay nhap a: ')
+a = int(input())
+print('Hay nhap b: ')
+b = int(input())
+if a == b:
+    print ("a = b")
+elif a < b :
+    print ('a < b')
+elif a > b : 
+    print ('a > b')
