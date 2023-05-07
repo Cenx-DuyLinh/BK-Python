0b94e1f72e780eee9505104da0c141b5b4d08e48
longnguyenvu2809 <vulong2809@gmail.com>
2023-05-07 21:11:06 +0700

    Update Baitap_8.py

--- BT_Long/Baitap_8.py
+++ BT_Long/Baitap_8.py
@@ -1,20 +1,39 @@
-# Check if file exist in current folder and delete
+# Week 1_Check if file exist in current folder and delete
+# Week 2_Add functions, parameters check using Try, Exception and Flags
+
 import os
+import sys
 
-# Input file's name to check
-name = input('Input file name: ')
+# Define function to get file name and return flag
+def get_file_name():
+    try:
+        file_name = input('Input file name to delete: ')
+        return file_name 
+    except:
+        print('Error input')
+        sys.exit() # Incase of error, return message and stop the program
 
-# Check and delete option
-if os.path.exists(name):
-    print(name ,'exists in current folder')
-    print('Do you want to delete ',name,'?')
-    flag = input('[y/n]: ') # Ask user whether delete or not
-    if flag == 'y':
-        os.remove(name)
-        print('File removed') # Delete command
-    elif flag == 'n':
-        print('End task.')
+# Define function to check if file exists and return flag
+def check_if_file_exists(file_name):
+    try:
+        return os.path.exists(file_name) 
+    except:
+        return False # return False if there's an error
+
+# Define function to delete file if exists and return flag
+def delete_file(file_name):
+    try:
+        os.remove(file_name)
+        return True # return True if the file is successfully deleted
+    except:
+        return False # return False if there's an error
+
+# Excecute function
+file_name = get_file_name()
+if check_if_file_exists(file_name):
+    if delete_file(file_name):
+        print("File deleted successfully.")
     else:
-        print('Inapporiate input -> End task.')
+        print("Error deleting file.")
 else:
-    print(name ,'does not exists in current folder')
+    print("File does not exist.") 
