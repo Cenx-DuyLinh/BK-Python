69ba08507460fa918e10e3836f3b25e9859db283
Duy Linh <duylinh.tran98@gmail.com>
2023-05-13 07:03:25 +0700

    thay doi ten file

--- code_test/test.py
+++ code_test/test.py
@@ -1,7 +1,16 @@
+def kiem_tra_file_ton_tai():
+    try:
+        import os
 
-try:
-    a = int(input())
-    b = 5/a
-    print(b)
-except:
-    print('cho nay bi loi')
+        ten_file = input('Hay nhap ten file de kiem tra: ')
+        vi_tri_file = os.getcwd()
+        list_cac_file = os.listdir(vi_tri_file)
+        
+
+        ket_qua_kiem_tra_file = ten_file in list_cac_file
+        if ket_qua_kiem_tra_file == 1 :
+            print('File nay co ton tai trong folder.')
+        elif ket_qua_kiem_tra_file == 0 :
+            print('File nay khong co ton tai trong folder.')
+    except: 
+        print('loi')
