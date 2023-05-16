04b58e73e349454ce1d06c9336a04b511302fd8a
Duy Linh <duylinh.tran98@gmail.com>
2023-05-16 17:59:15 +0700

    them folder test

--- code_test/test2.py
+++ code_test/test2.py
@@ -1,14 +1,18 @@
-
-import os
 def check_ten_path(ten_file):
     import os
     try:
         error_flag=False
         os.listdir(ten_file)
-    except :
-        print('hi')
+    except FileNotFoundError:
+        print("Xin hay nhap lai ten path !!!")
+        error_flag=True
+    return ten_file, error_flag
+
+import os
 
 ten_file = input('Hay nhap ten file de kiem tra: ')
+ten_file, error_flag = check_ten_path(ten_file)
+print(error_flag)
 #vi_tri_file = os.getcwd()
 list_cac_file = os.listdir(ten_file)
 print(list_cac_file)
