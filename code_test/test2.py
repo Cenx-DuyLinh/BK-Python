76edeac069189146d1459271728728d4959ddccc
Duy Linh <duylinh.tran98@gmail.com>
2023-05-16 17:40:09 +0700

    sua lai bai 7, them function check path

--- code_test/test2.py
+++ code_test/test2.py
@@ -1,4 +1,12 @@
+
 import os
+def check_ten_path(ten_file):
+    import os
+    try:
+        error_flag=False
+        os.listdir(ten_file)
+    except :
+        print('hi')
 
 ten_file = input('Hay nhap ten file de kiem tra: ')
 #vi_tri_file = os.getcwd()
