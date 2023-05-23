60ab4314b3139931127b44356f4b89206ee223fe
Duy Linh <duylinh.tran98@gmail.com>
2023-05-23 14:03:01 +0700

    them stuff

--- BT_Linh/duylinh_baitapIT_4/bai_tap_Graph.py
+++ BT_Linh/duylinh_baitapIT_4/bai_tap_Graph.py
@@ -5,22 +5,25 @@ def RUN():
     import random
     import matplotlib.pyplot as plt
     from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-    import pandas as pd
 
     #Create the window
     window = tk.Tk()
     window.geometry('600x500')
 
-    data_test = {'x_axis':[0,1,2,3,4,5],'y_axis':[2,4,5,2,4,7]}
-    data_frame = pd.DataFrame(data_test)
+
     figure_buffer = plt.Figure(figsize=(5,4),dpi=100)
     figure_plot = figure_buffer.add_subplot(1,1,1)
     #Place figure on main window
     line = FigureCanvasTkAgg(figure_buffer,window)
     # get_tk_widget
-    line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH) #position the graph
+    line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
     # Meant for actual dates - groups unemployment rates by year and adds them up
-    data_frame.plot(kind='line', legend=True, ax=figure_plot, color='r', marker='o', fontsize=10)
+
+
+    #Create the class
+    class graph:
+        def __init__(self,name) -> None:
+            self.name = name
 
     #Run the UI
     window.mainloop()
